8851e8f3b44af907dc2197747ee4bc49edfc883a
faiz <ansarifaiz32@gmail.com>
2020-04-25 00:41:48 +0530

    to deploy MVP

--- src/components/data.js
+++ src/components/data.js
@@ -7,178 +7,179 @@ const data = [
     },
     {
         "roza": "2",
-        "date": "27 Apr 2020",
+        "date": "26 Apr 2020",
         "sehr": "04:49 am",
         "iftaar": "07:03 pm"
     },
     {
         "roza": "3",
+        "date": "27 Apr 2020",
+        "sehr": "04:49 am",
+        "iftaar": "07:03 pm"
+    },
+    {
+        "roza": "4",
         "date": "28 Apr 2020",
         "sehr": "04:48 am",
         "iftaar": "07:04 pm"
     },
     {
-        "roza": "4",
+        "roza": "5",
         "date": "29 Apr 2020",
         "sehr": "04:47 am",
         "iftaar": "07:03 pm"
     },
     {
-        "roza": "5",
+        "roza": "6",
         "date": "30 Apr 2020",
         "sehr": "04:47 am",
         "iftaar": "07:04 pm"
     },
     {
-        "roza": "6",
+        "roza": "7",
         "date": "01 May 2020",
         "sehr": "04:46 am",
         "iftaar": "07:05 pm"
     },
     {
-        "roza": "7",
+        "roza": "8",
         "date": "02 May 2020",
         "sehr": "04:46 am",
         "iftaar": "07:05 pm"
     },
     {
-        "roza": "8",
+        "roza": "9",
         "date": "03 May 2020",
         "sehr": "04:45 am",
         "iftaar": "07:05 pm"
     },
     {
-        "roza": "9",
+        "roza": "10",
         "date": "04 May 2020",
         "sehr": "04:45 am",
         "iftaar": "07:06 pm"
     },
     {
-        "roza": "10",
+        "roza": "11",
         "date": "05 May 2020",
         "sehr": "04:44 am",
         "iftaar": "07:06 pm"
     },
     {
-        "roza": "11",
+        "roza": "12",
         "date": "06 May 2020",
         "sehr": "04:44 am",
         "iftaar": "07:07 pm"
     },
     {
-        "roza": "12",
+        "roza": "13",
         "date": "07 May 2020",
         "sehr": "04:43 am",
         "iftaar": "07:07 pm"
     },
     {
-        "roza": "13",
+        "roza": "14",
         "date": "08 May 2020",
         "sehr": "04:43 am",
         "iftaar": "07:07 pm"
     },
     {
-        "roza": "14",
+        "roza": "15",
         "date": "09 May 2020",
         "sehr": "04:42 am",
         "iftaar": "07:08 pm"
     },
     {
-        "roza": "15",
+        "roza": "16",
         "date": "10 May 2020",
         "sehr": "04:42 am",
         "iftaar": "07:08 pm"
     },
     {
-        "roza": "16",
+        "roza": "17",
         "date": "11 May 2020",
         "sehr": "04:41 am",
         "iftaar": "07:08 pm"
     },
     {
-        "roza": "17",
+        "roza": "18",
         "date": "12 May 2020",
         "sehr": "04:41 am",
         "iftaar": "07:09 pm"
     },
     {
-        "roza": "18",
+        "roza": "19",
         "date": "13 May 2020",
         "sehr": "04:41 am",
         "iftaar": "07:09 pm"
     },
     {
-        "roza": "19",
+        "roza": "20",
         "date": "14 May 2020",
         "sehr": "04:40 am",
         "iftaar": "07:10 pm"
     },
     {
-        "roza": "20",
+        "roza": "21",
         "date": "15 May 2020",
         "sehr": "04:40 am",
         "iftaar": "07:10 pm"
     },
     {
-        "roza": "21",
+        "roza": "22",
         "date": "16 May 2020",
         "sehr": "04:40 am",
         "iftaar": "07:10 pm"
     },
     {
-        "roza": "22",
+        "roza": "23",
         "date": "17 May 2020",
         "sehr": "04:39 am",
         "iftaar": "07:11 pm"
     },
     {
-        "roza": "23",
+        "roza": "24",
         "date": "18 May 2020",
         "sehr": "04:39 am",
         "iftaar": "07:11 pm"
     },
     {
-        "roza": "24",
+        "roza": "25",
         "date": "19 May 2020",
         "sehr": "04:39 am",
         "iftaar": "07:12 pm"
     },
     {
-        "roza": "25",
+        "roza": "26",
         "date": "20 May 2020",
         "sehr": "04:38 am",
         "iftaar": "07:12 pm"
     },
     {
-        "roza": "26",
+        "roza": "27",
         "date": "21 May 2019",
         "sehr": "04:38 am",
         "iftaar": "07:12 pm"
     },
     {
-        "roza": "27",
+        "roza": "28",
         "date": "22 May 2019",
         "sehr": "04:38 am",
         "iftaar": "07:13 pm"
     },
     {
-        "roza": "28",
+        "roza": "29",
         "date": "23 May 2019",
         "sehr": "04:38 am",
         "iftaar": "07:13 pm"
     },
     {
-        "roza": "29",
+        "roza": "30",
         "date": "24 May 2019",
         "sehr": "04:38 am",
         "iftaar": "07:13 pm"
     },
-    {
-        "roza": "30",
-        "date": "25 May 2019",
-        "sehr": "04:38 am",
-        "iftaar": "07:13 pm"
-    },
+    
 ]
 
 export default data
