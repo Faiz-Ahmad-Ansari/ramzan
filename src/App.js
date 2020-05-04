562f5d409a87cfd0e5b51051dc53e26150e3692a
qaispalekar <qais@symboinsurance.com>
2020-05-04 23:47:46 +0530

    added code splitting in the route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import TimeTable from './components/timetable'
-import Quran from './components/quran'
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+// import TimeTable from './components/timetable'
+// import Quran from './components/quran'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+const TimeTable = lazy(() => import('./components/timetable'));
+const Quran = lazy(() => import('./components/quran'));
 function App() {
   return (
     <div >
       <BrowserRouter>
-       <Switch>
-         <Route path='/' exact component = {TimeTable} />
-         <Route path='/quran'  component = {Quran} />
-          {/* <TimeTable />
-          <Quran /> */}
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path='/' exact component={TimeTable} />
+            <Route path='/quran' component={Quran} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
