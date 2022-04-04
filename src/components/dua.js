8b90ae89b50cfcdec527f34bdfb66631c805dd84
Faiz Ansari <faiz.ansari@badbeat.com>
2022-04-05 03:21:52 +0530

    dua to arabic

--- src/components/dua.js
+++ src/components/dua.js
@@ -17,9 +17,13 @@ function Dua() {
                                                 
                     <div className="text-center">
                         <div className='text-left font-weight-bold mt-2 pt-5 pl-3'>Dua For Sehr & Iftar</div>
-                        <div className="row  m-0 p-0 m-0 p-0 pt-2 pb-1">
+                            <div className="row  m-0 p-0 m-0 p-0 pt-2 pb-1">
                                 <div className="col-4 text-left pl-3 font-weight-bold ">Sehr</div>
-                                <div className="col-8 text-left font-weight-bold"><i> "Wa bisawmi ghadinn nawaiytu min shahri Ramadan"</i></div>
+                                <div className="col-8 text-left font-weight-bold"><h4> وَبِصَوْمِ غَدٍ نَّوَيْتُ مِنْ شَهْرِ رَمَضَانَ</h4></div>
+                            </div>
+                            <div className="row  m-0 p-0 m-0 p-0 pt-2 pb-1">
+                                <div className="col-4 text-left pl-3  "></div>
+                                <div className="col-8 text-left "><i> "Wa bisawmi ghadinn nawaiytu min shahri Ramadan"</i></div>
                             </div>
                             <div className="row border-bottom-grey m-0 p-0 pt-1 pb-2">
                                 <div className="col-4 text-left pl-3 ">Translation</div>
@@ -27,7 +31,11 @@ function Dua() {
                             </div>
                             <div className="row  m-0 p-0 pt-2 pb-1">
                                 <div className="col-4 text-left pl-3 font-weight-bold">Iftar</div>
-                                <div className="col-8 text-left font-weight-bold"><i> "Allahumma inni laka sumtu, wa bika aamantu, [wa ‘alayka tawakkaltu], wa Ala rizqika aftartu"</i></div>
+                                <div className="col-8 text-left font-weight-bold"><h4>اَللّٰهُمَّ اِنِّی لَکَ صُمْتُ وَبِکَ اٰمَنْتُ وَعَلَيْکَ تَوَکَّلْتُ وَعَلٰی رِزْقِکَ اَفْطَرْتُ </h4></div>
+                            </div>
+                            <div className="row  m-0 p-0 pt-2 pb-1">
+                                <div className="col-4 text-left pl-3 "></div>
+                                <div className="col-8 text-left "><i> "Allahumma inni laka sumtu, wa bika aamantu, [wa ‘alayka tawakkaltu], wa Ala rizqika aftartu"</i></div>
                             </div>
                             <div className="row border-bottom-grey m-0 p-0 pt-1 pb-2">
                                 <div className="col-4 text-left pl-3 ">Translation</div>
@@ -37,23 +45,23 @@ function Dua() {
                         <div className='text-left font-weight-bold pt-2 pl-3 font-weight-bold'>Dua for Three Ashras</div>
                         <div className="row  m-0 p-0 m-0 p-0 pt-2 pb-1">
                                 <div className="col-4 text-left pl-3 font-weight-bold ">First Ashra</div>
-                                <div className="col-8 text-left font-weight-bold"><i> "Rabbi ighfir warham wa anta khayrur raahimeen"</i></div>
+                                <div className="col-8 text-left font-weight-bold"><h4> يَا حَيُّ يَا قَيُّومُ بِرَحْمَتِكَ أَسْتَغيثُ</h4></div>
                             </div>
                             <div className="row border-bottom-grey m-0 p-0 pt-1 pb-2">
                                 <div className="col-4 text-left pl-3 ">Translation</div>
-                                <div className="col-8 text-left">O my Lord! Forgive me and have mercy and You are the best, most merciful.</div>
+                                <div className="col-8 text-left">Oh Everliving, The Everlasting, I seek Your help through Your mercy.</div>
                             </div>
                             <div className="row  m-0 p-0 pt-2 pb-1">
                                 <div className="col-4 text-left pl-3 font-weight-bold">Second Ashra</div>
-                                <div className="col-8 text-left font-weight-bold"><i> "Astagfirullaha rab-bi min kulli zambiyon wa-atoobuilaiyh"</i></div>
+                                <div className="col-8 text-left font-weight-bold"><h4> اَسْتَغْفِرُ اللہَ رَبِّی مِنْ کُلِّ زَنْبٍ وَّ اَتُوْبُ اِلَیْہِ</h4></div>
                             </div>
                             <div className="row border-bottom-grey m-0 p-0 pt-1 pb-2">
                                 <div className="col-4 text-left pl-3 ">Translation</div>
-                                <div className="col-8 text-left">I ask for forgiveness from Allah, my Lord, for every sin I have committed. </div>
+                                <div className="col-8 text-left">I seek forgiveness from Allah for all my sins and turn to Him. </div>
                             </div>
                             <div className="row  m-0 p-0 pt-2 pb-1">
                                 <div className="col-4 text-left pl-3 font-weight-bold">Third Ashra</div>
-                                <div className="col-8 text-left font-weight-bold"><i> "Allahumma Ajirni minan naar"</i></div>
+                                <div className="col-8 text-left font-weight-bold"><h4> اَللَّهُمَّ أَجِرْنِي مِنَ النَّارِ</h4></div>
                             </div>
                             <div className="row border-bottom-grey m-0 p-0 pt-1 pb-2">
                                 <div className="col-4 text-left pl-3 ">Translation</div>
